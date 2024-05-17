cc37175699637bc8af0532fcc4d2d4394ce95aac
umangUS002 <hsoni9636@gmail.com>
2024-05-17 17:37:16 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,22 @@
+
+document.querySelector("a").onclick= myFunction();
+
+function myFunction(){
+var randomNum1 = Math.floor(Math.random()*6)+1;
+var img1src = "./images/dice"+randomNum1+".png";
+
+document.querySelectorAll("img")[0].setAttribute("src",img1src);
+
+var randomNum2 = Math.floor(Math.random()*6)+1;
+var img2src = "./images/dice"+randomNum2+".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",img2src);
+
+if(randomNum1>randomNum2){
+    document.querySelector("h2").innerHTML = "Player 1 Won!🥳";
+} else if(randomNum1<randomNum2){
+    document.querySelector("h2").innerHTML = "Player 2 Won!🤩";
+} else{
+    document.querySelector("h2").innerHTML = "Draw!😅";
+}
+}
